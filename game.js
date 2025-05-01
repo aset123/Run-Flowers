92ea6ef9ec8685d0c6c086fae8728831647f973a
aset123 <61176113+aset123@users.noreply.github.com>
2025-05-02 00:30:24 +0500

    Update game.js

--- game.js
+++ game.js
@@ -59,7 +59,7 @@ window.addEventListener("load", () => {
     function createFlower() {
       flowers.push({
         x: canvas.width,
-        y: groundY - 80,
+        y: groundY - 75,
         width: 70,
         height: 75
       });
